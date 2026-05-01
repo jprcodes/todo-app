8b2f30f42c7121249debee5c4faf2460dde9b428
Jonnalagadda PushpaRaju <jsjonnalagadda90@gmail.com>
2026-05-01 15:54:52 +0900

    first todo app

---
+++ script.js
@@ -0,0 +1,23 @@
+function addTask() {
+  let input = document.getElementById("taskInput");
+  let task = input.value;
+
+  if (task === "") {
+    alert("Enter a task");
+    return;
+  }
+
+  let li = document.createElement("li");
+  li.innerText = task;
+
+  let btn = document.createElement("button");
+  btn.innerText = "Delete";
+  btn.onclick = function () {
+    li.remove();
+  };
+
+  li.appendChild(btn);
+  document.getElementById("taskList").appendChild(li);
+
+  input.value = "";
+}
